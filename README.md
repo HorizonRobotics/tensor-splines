5f06696eafca74e41d6f57fea2bda4e9513d3b04
Break Yang <breakds@gmail.com>
2022-06-07 14:48:02 -0700

    Add pure nix development environment

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tensor-splines
 An utility library to generate splines with tensor-based batch operation support.
+
+## Development Environment
+
+There are two `devShell` that you can invoke to get a development environment.
+
+1. For the pure Nix-based development environment, run
+
+   ```bash
+   $ nix develop
+   ```
+   
+2. For `poetry`-based development environment, run
+
+   ```bash
+   $ nix develop .#poetry
+   ```
+
+   And you will have the command `poetry`. The rest should follow your normal poetry workflow.
--- flake.nix
+++ flake.nix
@@ -4,29 +4,64 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
 
-    flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";    
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+
+    ml-pkgs.url = "github:nixvital/ml-pkgs";
+    ml-pkgs.inputs.nixpkgs.follows = "nixpkgs";
+    ml-pkgs.inputs.utils.follows = "utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachSystem [
+  outputs = { self, nixpkgs, utils, ... }@inputs: utils.lib.eachSystem [
     "x86_64-linux"
   ] (system: let
     pkgs = import nixpkgs {
       inherit system;
       config.allowUnfree = true;
+      overlays = [(final: prev: rec {
+        python3 = prev.python3.override {
+          packageOverrides = pyFinal: pyPrev : rec {
+            inherit (inputs.ml-pkgs.packages."${system}")
+              pytorchWithCuda11;
+          };
+        };
+        python3Packages = python3.pkgs;
+      })];
     };
   in {
-    devShells.default = pkgs.mkShell rec {
-      name = "tensor-splines";
-      packages = with pkgs; [
-        poetry
-      ];
-      shellHook = ''
-        export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-        export PYTHONPATH="$(pwd):$PYTHONPATH"
-        # In poetry environment, packages such as numpy needs them.
-        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.zlib}/lib/:$LD_LIBRARY_PATH"
+    devShells = {
+      default = pkgs.mkShell rec {
+        name = "tensor-splines";
+
+        packages = let pythonEnv = pkgs.python3.withPackages (pyPkgs: with pyPkgs; [
+          numpy
+          matplotlib
+          pytorchWithCuda11
+        ]); in [
+          pythonEnv
+          # Dev Tools
+          pkgs.nodePackages.pyright
+          pkgs.pre-commit
+        ];
+
+        shellHook = ''
+          export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+          export PYTHONPATH="$(pwd):$PYTHONPATH"
+        '';
+      };
+
+      poetry = pkgs.mkShell rec {
+        name = "tensor-splines";
+        packages = with pkgs; [
+          poetry
+        ];
+        shellHook = ''
+          export PS1="$(echo -e '\uf3e2') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+          export PYTHONPATH="$(pwd):$PYTHONPATH"
+          # In poetry environment, packages such as numpy needs them.
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.zlib}/lib/:$LD_LIBRARY_PATH"
       '';
+      };
     };
   });
 }
